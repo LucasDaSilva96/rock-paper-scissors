5bc4ac2354a08b4758f441d35411c4a4429ba19f
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-05-08 12:34:26 +0200

    Created the simulator function that simulates a single round of the game.

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ const get_computer_choice = function (arr) {
 // A global variable to keep track of the user's choice.
 let player_weapon = "";
 
+// Global Variables to keep track of the points after each round.
+let computer_points = 0;
+let player_points = 0;
+
 // Prompt the user to make a choice from the choice_array, and the user can not input a higher value then 3 or below 1 - Function
 const player_choice = function (arr) {
   let choice;
@@ -23,16 +27,92 @@ const player_choice = function (arr) {
   do {
     choice = Number(
       prompt(
-        `Please choose your weapon: 1: ${arr[0].toUpperCase()}, 2: ${arr[1].toUpperCase()}, 3: ${arr[2].toUpperCase()}.`
+        `Please choose your weapon:
+        1: ${arr[0].toUpperCase()}
+        2: ${arr[1].toUpperCase()}
+        3: ${arr[2].toUpperCase()}`
       )
     );
   } while (choice < 1 || choice > 3);
 
   // The nr that the user input must be reduce by 1 because array's starts at index 0 and not 1.
-  player_weapon = arr[choice - 1].toLowerCase();
+  return (player_weapon = arr[choice - 1].toLowerCase());
 };
 
 // Call the function
 player_choice(choice_array);
 
-console.log(player_weapon);
+// A function that plays a single round of Rock Paper Scissors.
+
+const game_simulator = function (computerSelection, playerSelection) {
+  // Computer's choice = paper
+  if (computerSelection === "paper" && playerSelection === "rock") {
+    computer_points++;
+    console.log(
+      `You Lose!, ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+    );
+  }
+
+  // Computer's choice = paper
+  if (computerSelection === "paper" && playerSelection === "scissors") {
+    player_points++;
+    console.log(
+      `You Win!, ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+    );
+  }
+
+  // Computer's choice = paper
+  if (computerSelection === "paper" && playerSelection === "paper") {
+    console.log(`Draw!`);
+  }
+
+  // Computer's choice = rock
+  if (computerSelection === "rock" && playerSelection === "rock") {
+    console.log(`Draw!`);
+  }
+
+  // Computer's choice = rock
+  if (computerSelection === "rock" && playerSelection === "scissors") {
+    computer_points++;
+    console.log(
+      `You Lose!, ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+    );
+  }
+
+  // Computer's choice = rock
+  if (computerSelection === "rock" && playerSelection === "paper") {
+    player_points++;
+    console.log(
+      `You Win!, ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+    );
+  }
+
+  // Computer's choice = scissors
+  if (computerSelection === "scissors" && playerSelection === "paper") {
+    computer_points++;
+    console.log(
+      `You Lose!, ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+    );
+  }
+
+  // Computer's choice = scissors
+  if (computerSelection === "scissors" && playerSelection === "rock") {
+    player_points++;
+    console.log(
+      `You Win!, ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+    );
+  }
+
+  // Computer's choice = scissors
+  if (computerSelection === "scissors" && playerSelection === "scissors") {
+    console.log(`Draw!`);
+  }
+};
+
+// Call the game_simulator function with the computer and player's choice.
+game_simulator(get_computer_choice(choice_array), player_weapon);
+
+console.log("Computer-points: " + computer_points);
+console.log("Player-points: " + player_points);
+
+// The game function - play a 5 round game that keeps score and reports a winner or loser at the end.
