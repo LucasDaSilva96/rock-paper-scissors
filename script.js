74183c4ea7da90c17604359efa7ad0be411f3d7e
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-05-13 21:34:40 +0200

    Add more code inside the game_simulator function for better UI and created the point_function to keep track of the score.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // DOM - selections
+const h1 = document.querySelector("h1");
 const play_btn = document.querySelector(".play-btn");
 const quote_text_box = document.querySelector(".quote");
 const robot_img = document.querySelector(".robot-img");
@@ -37,6 +38,12 @@ const quotes_array_wrong = [
   "We need help!",
   "AI is taking over...",
 ];
+
+// Random Quote generator
+const quote_generator = function (arr) {
+  let random_nr = Math.floor(Math.random() * arr.length);
+  return arr[random_nr];
+};
 // The array that contains the different choices that the computer can make.
 const choice_array = ["paper", "rock", "scissors"];
 
@@ -46,24 +53,12 @@ const get_computer_choice = function (arr) {
   let random_nr = Math.floor(Math.random() * arr.length);
 
   // Return the array at the current random_nr position.
+
   return arr[random_nr];
 };
 
-// A global variable to keep track of the user's choice.
+// A global variable to keep track of the user's choice and the computer's choice.
 let player_weapon = "";
-
-// Get player's choice
-pistol_choice.addEventListener("click", function () {
-  player_weapon = "rock";
-});
-
-shotgun_choice.addEventListener("click", function () {
-  player_weapon = "paper";
-});
-
-sniper_choice.addEventListener("click", function () {
-  player_weapon = "scissors";
-});
 
 // Global Variables to keep track of the points after each round.
 let computer_points = 0;
@@ -78,6 +73,8 @@ const game_simulator = function (computerSelection, playerSelection) {
     ai_score_p.textContent = `${computer_points}`;
     ai_score_box.classList.add("shadow-show");
     human_score_box.classList.remove("shadow-show");
+    quote_text_box.textContent = quote_generator(quotes_array_wrong);
+    point_tracker(computer_points, player_points);
   }
 
   // Computer's choice = paper
@@ -86,20 +83,24 @@ const game_simulator = function (computerSelection, playerSelection) {
     human_score_p.textContent = `${player_points}`;
     human_score_box.classList.add("shadow-show");
     ai_score_box.classList.remove("shadow-show");
+    quote_text_box.textContent = quote_generator(quotes_array_correct);
+    point_tracker(computer_points, player_points);
   }
 
   // Computer's choice = paper
   if (computerSelection === "paper" && playerSelection === "paper") {
-    quote_text_box.textContent = "You chose the same weapon as the AI";
+    quote_text_box.textContent = "Same choose as the AI...";
     ai_score_box.classList.remove("shadow-show");
     human_score_box.classList.remove("shadow-show");
+    point_tracker(computer_points, player_points);
   }
 
   // Computer's choice = rock
   if (computerSelection === "rock" && playerSelection === "rock") {
-    quote_text_box.textContent = "You chose the same weapon as the AI";
+    quote_text_box.textContent = "Same choose as the AI...";
     ai_score_box.classList.remove("shadow-show");
     human_score_box.classList.remove("shadow-show");
+    point_tracker(computer_points, player_points);
   }
 
   // Computer's choice = rock
@@ -108,6 +109,8 @@ const game_simulator = function (computerSelection, playerSelection) {
     ai_score_p.textContent = `${computer_points}`;
     ai_score_box.classList.add("shadow-show");
     human_score_box.classList.remove("shadow-show");
+    quote_text_box.textContent = quote_generator(quotes_array_wrong);
+    point_tracker(computer_points, player_points);
   }
 
   // Computer's choice = rock
@@ -116,6 +119,8 @@ const game_simulator = function (computerSelection, playerSelection) {
     human_score_p.textContent = `${player_points}`;
     human_score_box.classList.add("shadow-show");
     ai_score_box.classList.remove("shadow-show");
+    quote_text_box.textContent = quote_generator(quotes_array_correct);
+    point_tracker(computer_points, player_points);
   }
 
   // Computer's choice = scissors
@@ -124,6 +129,8 @@ const game_simulator = function (computerSelection, playerSelection) {
     ai_score_p.textContent = `${computer_points}`;
     ai_score_box.classList.add("shadow-show");
     human_score_box.classList.remove("shadow-show");
+    quote_text_box.textContent = quote_generator(quotes_array_wrong);
+    point_tracker(computer_points, player_points);
   }
 
   // Computer's choice = scissors
@@ -132,40 +139,79 @@ const game_simulator = function (computerSelection, playerSelection) {
     human_score_p.textContent = `${player_points}`;
     human_score_box.classList.add("shadow-show");
     ai_score_box.classList.remove("shadow-show");
+    quote_text_box.textContent = quote_generator(quotes_array_correct);
+    point_tracker(computer_points, player_points);
   }
 
   // Computer's choice = scissors
   if (computerSelection === "scissors" && playerSelection === "scissors") {
-    quote_text_box.textContent = "You chose the same weapon as the AI";
+    quote_text_box.textContent = "Same choose as the AI...";
     ai_score_box.classList.remove("shadow-show");
     human_score_box.classList.remove("shadow-show");
+    point_tracker(computer_points, player_points);
   }
 };
-
-// Call the game_simulator function with the computer and player's choice.
-game_simulator(get_computer_choice(choice_array), player_weapon);
 
 // The game function - play a 5 round game that keeps score and reports a winner or loser at the end.
-
-const game = function (rounds = 5) {
-  let nr_of_rounds = rounds;
-
-  for (let i = 1; i <= nr_of_rounds; i++) {
-    // player_choice(choice_array);
-    game_simulator(get_computer_choice(choice_array), player_weapon);
+const point_tracker = function (computer_points, player_points) {
+  if (computer_points === 5) {
+    h1.textContent = "You LOST the game...";
+    player_points = 0;
+    computer_points = 0;
+    human_score_p.textContent = `${player_points}`;
+    ai_score_p.textContent = `${computer_points}`;
+    play_btn.textContent = "Play Again";
+    play_btn.classList.remove("no-show");
+    quote_text_box.classList.add("no-show");
+    weapons_container.classList.add("no-show");
   }
-
-  if (computer_points > player_points) {
-    console.log(
-      `Computer is the WINNER. Score [${computer_points} - ${player_points}]`
-    );
-  } else if (computer_points < player_points) {
-    console.log(
-      `Player is the WINNER. Score [${player_points} - ${computer_points}]`
-    );
-  } else {
-    console.log("DRAW...");
+  if (player_points === 5) {
+    h1.textContent = "You are the WINNER!";
+    player_points = 0;
+    computer_points = 0;
+    human_score_p.textContent = `${player_points}`;
+    ai_score_p.textContent = `${computer_points}`;
+    play_btn.textContent = "Play Again";
+    play_btn.classList.remove("no-show");
+    quote_text_box.classList.add("no-show");
+    weapons_container.classList.add("no-show");
   }
 };
 
-game();
+// Get player's choice
+const player_choice = function () {
+  const boxes = document.querySelectorAll(".choice-box");
+  let rounds = 1;
+  boxes.forEach((el) => {
+    el.addEventListener("click", function () {
+      h1.textContent = `Round: ${rounds}`;
+      rounds++;
+      if (el.classList.contains("rock")) {
+        player_weapon = "rock";
+
+        return game_simulator(get_computer_choice(choice_array), player_weapon);
+      }
+
+      if (el.classList.contains("paper")) {
+        player_weapon = "paper";
+
+        return game_simulator(get_computer_choice(choice_array), player_weapon);
+      }
+
+      if (el.classList.contains("scissors")) {
+        player_weapon = "scissors";
+
+        return game_simulator(get_computer_choice(choice_array), player_weapon);
+      }
+    });
+  });
+};
+
+// The Play function
+play_btn.addEventListener("click", function () {
+  play_btn.classList.add("no-show");
+  quote_text_box.classList.remove("no-show");
+  weapons_container.classList.remove("no-show");
+
+  player_choice();
+});
